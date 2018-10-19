7eb2c7cfd9303e19ad12fc0ef3053f26792f2077
Philipp Waibel <>
2018-10-19 11:09:06 +0200

    save sensor information on sensor-hardware

--- sensor-hardware/src/main/java/at/piwa/thermometer/sensor/bootstrap/sensortypes/SensorsWriteImpl.java
+++ sensor-hardware/src/main/java/at/piwa/thermometer/sensor/bootstrap/sensortypes/SensorsWriteImpl.java
@@ -1,14 +1,13 @@
 package at.piwa.thermometer.sensor.bootstrap.sensortypes;
 
 import at.piwa.thermometer.sensor.domain.Sensor;
-import com.sun.javafx.scene.shape.PathUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.util.FileSystemUtils;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
 import java.net.URISyntaxException;
@@ -20,30 +19,20 @@ import java.util.List;
  */
 @Component
 @Slf4j
-public class SensorsReaderImpl {
+public class SensorsWriteImpl {
 
     @Value("${sensor.configurations.path}")
     private String sensorConfigurationsPath;
-    @Value("${sensor.configurations.original.path}")
-    private String sensorConfigurationsOriginalPath;
 
-    public List<Sensor> readSensorConfigurations() {
+    public void writeSensorConfigurations(SensorConfigurations sensorConfigurations) {
         try {
             JAXBContext jaxbContext = JAXBContext.newInstance( SensorConfigurations.class );
-            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-
+            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
             File file = Paths.get(this.getClass().getClassLoader().getResource(sensorConfigurationsPath).toURI()).toFile();
-            if(!file.exists()) {
-                file = Paths.get(this.getClass().getClassLoader().getResource(sensorConfigurationsOriginalPath).toURI()).toFile();
-            }
+            jaxbMarshaller.marshal(sensorConfigurations, file);
 
-            SensorConfigurations sensorConfigurations = (SensorConfigurations) jaxbUnmarshaller.unmarshal(file);
-
-            return sensorConfigurations.getSensors();
         } catch (JAXBException | URISyntaxException e) {
             log.error("Exception", e);
         }
-
-        return null;
     }
 }
